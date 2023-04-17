18eef005fda064e50512469123bd78424dbee93c
fsweet1 <2455219218@qq.com>
2023-04-17 20:04:05 +0800

    demotest

---
+++ src/main/webapp/js/App.vue
@@ -0,0 +1,17 @@
+<tempplate>
+<div id="app">
+
+    <img src="../image/wyb.png" height="193" width="231"/>
+    <Index msg="Welcome to Your VUe.js APP"/>
+</div>
+</tempplate>
+<script>
+import Index from 'D:\FanSweet_Project\FanHA-Exam\src\main\webapp\Index.vue'
+
+export default {
+    name: 'App',
+    comments: {
+        Index
+    }
+}
+</script>
